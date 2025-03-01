8b0c853f340e7c25ff42f59ad578ba78d1c67937
lundiko <petrawaterstreet@ip0af5dbba.int.uvm.edu>
2025-03-01 14:34:43 -0500

    node but with css cause we're cool like that or whatever :3

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Serve static files like images, CSS, and JS
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Set the view engine to EJS for HTML templating
+app.set('view engine', 'ejs');
+
+// Define routes
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.get('/donations', (req, res) => {
+    res.render('donations');
+});
+
+app.get('/trades', (req, res) => {
+    res.render('trades');
+});
+
+app.get('/buy', (req, res) => {
+    res.render('buy');
+});
+
+app.get('/sell', (req, res) => {
+    res.render('sell');
+});
+
+app.get('/signup', (req, res) => {
+    res.render('signup');
+});
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
